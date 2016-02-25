dbeca9b935bd790b82483e28646d21c08145e5c1
Jennie Chu <jenniechu92@gmail.com>
2016-02-24 21:24:06 -0800

    RecipieController created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('recipes', []);
+(function() {
+	var app = angular.module('recipeBook', []);
+
+	app.controller('RecipeController',function(){
+		this.products = dinnerRecipes;
+	});
+
+	var dinnerRecipes = [
+
+	{
+		name: 'Honey Pork Belly',
+		ingredients: 'Pork Belly, Honey, Soy Sauce',
+		picture: '',
+	},
+	{
+		name: 'Teriyaki Chicken Wings',
+		ingredients: 'Chicken wings, Teriyaki Sauce',
+		picture: '',
+	},
+	
+	]
+
+})();
