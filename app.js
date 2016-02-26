784831dc85ac2d7eb5fec625f08aa8bb217327e1
Jennie Chu <jenniechu92@gmail.com>
2016-02-25 19:14:01 -0800

    fixed bug with photos broken

--- app.js
+++ app.js
@@ -10,14 +10,13 @@
 	{
 		name: 'Honey Pork Belly',
 		ingredients: 'Pork Belly, Honey, Soy Sauce',
-		images: [
-			'../images/honeyporkbelly.jpg',
-		],
+		images: "images/honeyporkbelly.jpg",
+		
 	},
 	{
 		name: 'Teriyaki Chicken Wings',
 		ingredients: 'Chicken wings, Teriyaki Sauce',
-		images: "../images/teriyakichicken.jpg",
+		images: "images/teriyakichicken.jpg",
 	},
 	
 	]
